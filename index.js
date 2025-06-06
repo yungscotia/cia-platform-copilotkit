e6102a5984e47d9d3af8a5405fd1ff94d8316860
nquaoser <nquaoser@netflix.com>
2025-06-06 11:55:29 -0700

    adding old export compatibility

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+// First, load all the packages
+const runtime = require('./CopilotKit/packages/runtime/dist/index.js');
+const reactCore = require('./CopilotKit/packages/react-core/dist/index.js');
+const reactTextarea = require('./CopilotKit/packages/react-textarea/dist/index.js');
+const reactUi = require('./CopilotKit/packages/react-ui/dist/index.js');
+const runtimeClientGql = require('./CopilotKit/packages/runtime-client-gql/dist/index.js');
+const sdkJs = require('./CopilotKit/packages/sdk-js/dist/index.js');
+const shared = require('./CopilotKit/packages/shared/dist/index.js');
+
 // Export all CopilotKit packages
 module.exports = {
-  runtime: require('./CopilotKit/packages/runtime/dist/index.js'),
-  reactCore: require('./CopilotKit/packages/react-core/dist/index.js'),
-  reactTextarea: require('./CopilotKit/packages/react-textarea/dist/index.js'),
-  reactUi: require('./CopilotKit/packages/react-ui/dist/index.js'),
-  runtimeClientGql: require('./CopilotKit/packages/runtime-client-gql/dist/index.js'),
-  sdkJs: require('./CopilotKit/packages/sdk-js/dist/index.js'),
-  shared: require('./CopilotKit/packages/shared/dist/index.js')
+  runtime,
+  reactCore,
+  reactTextarea,
+  reactUi,
+  runtimeClientGql,
+  sdkJs,
+  shared
 };
+
+// Also export the packages with their original names for backward compatibility
+module.exports['@copilotkit/runtime'] = runtime;
+module.exports['@copilotkit/react-core'] = reactCore;
+module.exports['@copilotkit/react-textarea'] = reactTextarea;
+module.exports['@copilotkit/react-ui'] = reactUi;
+module.exports['@copilotkit/runtime-client-gql'] = runtimeClientGql;
+module.exports['@copilotkit/sdk-js'] = sdkJs;
+module.exports['@copilotkit/shared'] = shared;
